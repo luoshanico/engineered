c94ab0b1945a5e2b0e3d7dfd5b19c931f3199d60
luoshanico <nicoworrall55@gmail.com>
2025-03-16 13:13:01 +0800

    removed restriction that max anchors = 2

--- src/builder/objects.py
+++ src/builder/objects.py
@@ -73,9 +73,6 @@ class Objects:
     def distance(self, p1, p2):
         return math.hypot(p1[0]-p2[0], p1[1] - p2[1])
 
-
-
-
     def clear_anchor_selection(self):
         self.selected_anchor = None
 
